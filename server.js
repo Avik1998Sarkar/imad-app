0b1d03ce3ac6b86ba0138eedbf9776c0ab6fdc9c
Avik1998Sarkar <36466256+Avik1998Sarkar@users.noreply.github.com>
2018-02-16 10:04:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+app.get('/ui/index.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
 
 var counter = 0;
 app.get('/counter',function(req,res){
